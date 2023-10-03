81d1c66bf724a3105a6cde453cca7205fee1f4bb
Ayoub <ay.belkacem@gmail.com>
2023-10-03 19:54:03 +0100

    print numbers combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(0, 9):
-        if (j > i and j != i):
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (j > i and j != i and i < 7 and j < 9):
             print("{}{}".format(i, j), end=", ")
 print("79, 89")
