ed7a30d6432ed6a439b898ed526369d7c56b2a92
Ayoub <ay.belkacem@gmail.com>
2023-10-24 18:05:47 +0100

    2-sqaure

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""This a class square"""
+
+
+class Square:
+    """ Define a square by its size"""
+
+    def __init__(self, size=0):
+        """ Method to initialize the square object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
